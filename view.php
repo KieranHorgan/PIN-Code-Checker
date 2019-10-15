ab16d4d621d3a0e69896ef2d5cb2c0ec301b1d07
KieranHorgan <kieranhorganmallow@gmail.com>
2019-10-15 16:08:09 +0100

    Added view.php

---
+++ view.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "mysql.netsoc.co";
+$username = "kieran";
+$password = "wFyzRAAAWI0";
+$dbname = "kieran_pin_codes";
+$table = "open_day_pin_codes";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$query  = "SELECT * FROM {$table} WHERE counter > 0 ORDER BY counter DESC, pin ASC;";
+$result = $conn->query($query);
+
+echo "<table>"; // start a table tag in the HTML
+echo "<tr><th>PIN</th><th>Count</th></tr>";
+while($row = $result->fetch_assoc()){   //Creates a loop to loop through results
+echo "<tr><td>" . $row['pin'] . "</td><td>" . $row['counter'] . "</td></tr>";  //$row['index'] the index here is a field name
+}
+
+echo "</table>"; //Close the table in HTML
+
+$conn->close();
+?>
